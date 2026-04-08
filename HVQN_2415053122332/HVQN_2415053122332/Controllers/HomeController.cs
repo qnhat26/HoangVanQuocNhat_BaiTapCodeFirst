bc7362d213440959fea550c7a131cb0f61237c3e
Hoang Van Quoc Nhat <hoangnhat250606@gmail.com>
2026-04-08 15:42:11 +0700

    Tao project moi

---
+++ HVQN_2415053122332/HVQN_2415053122332/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using HVQN_2415053122332.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HVQN_2415053122332.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
